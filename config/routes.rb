6737207eec02f58ddd3d7069d68b7540e6564762
Zachary Cohen <zachary@zacharyc.com>
2023-10-17 20:20:00 -0400

    feat: updating the invoice routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "invoices#index"
 
   resources :invoices
+  resources :customers
   # get "/invoices", to: "invoices#index"
   # get "/invoices/:id", to: "invoices#show"
 end
--- db/migrate/20231017230933_add_customer_to_invoice.rb
+++ db/migrate/20231017230933_add_customer_to_invoice.rb
@@ -1,4 +1,5 @@
 class AddCustomerToInvoice < ActiveRecord::Migration[7.1]
   def change
+    add_reference :invoice, :customer, foreign_key: true
   end
 end
